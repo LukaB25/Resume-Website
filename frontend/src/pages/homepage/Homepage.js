a0ae0a42e372cbd3ea0b6f9fba6fea6753b4aa02
Luka Black <luka.black20@gmail.com>
2024-06-18 16:39:25 +0000

    feat: Add homepage details to be read and displayed from the API, minor changes to styles

--- frontend/src/pages/homepage/Homepage.js
+++ frontend/src/pages/homepage/Homepage.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/esm/Col';
+
+import { usePersonalInfo } from '../../contexts/PersonalInfoContext';
+
+import styles from '../../styles/Homepage.module.css';
 function Homepage() {
+  const personalInfo = usePersonalInfo().data?.[0];
+
+  const splitParagraphs = personalInfo?.summary.split('\n');
+  const paragraphs = splitParagraphs?.map((paragraph, index) => {
+    return (
+      <Col key={index} className={styles.SummaryParagraph}>{paragraph}</Col>
+    )
+  });
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+      <Row className={`${styles.SummaryContainer} ${styles.SummarySmallScreen}`}>
+          {paragraphs}
+      </Row>
   )
 }
 
